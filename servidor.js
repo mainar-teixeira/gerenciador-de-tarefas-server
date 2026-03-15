f7351222b4ccedf7de4f50fc8dde3a8518b614e0
Mainar <mainar800@gmail.com>
2026-03-15 13:38:47 -0300

    upgrade geral

--- servidor.js
+++ servidor.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mysql = require('mysql2');
 
+require('dotenv').config();
 
 const conexaoBD = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "190292192",
-    database: "tarefas"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 const app = express();
