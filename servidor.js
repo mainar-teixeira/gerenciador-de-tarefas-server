e488e35b578f5118ea7d065f0901d16ab40a74ec
Mainar <mainar800@gmail.com>
2026-03-15 17:46:06 -0300

    server addicted

--- servidor.js
+++ servidor.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.DB_PORT || 3000;
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL
